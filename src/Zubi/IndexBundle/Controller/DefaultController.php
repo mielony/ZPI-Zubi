a293a038653ed348a92f8d562b6f4410facd98fd
Lukasz Wojciechowski <nostrzak@gmail.com>
2012-04-13 21:16:53 +0200

    Koniec R02/S01

--- src/Zubi/IndexBundle/Controller/DefaultController.php
+++ src/Zubi/IndexBundle/Controller/DefaultController.php
@@ -3,24 +3,23 @@
 namespace Zubi\IndexBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-
+use Zubi\DeviceBundle\Entity\Measurement;
+use Zubi\DeviceBundle\Entity\Station;
+use Symfony\Component\Security\Core\SecurityContext;
 class DefaultController extends Controller
 {
     
-    public function indexAction()
-    {
+    public function indexAction() {
 
-    	$session = $this->getRequest()->getSession();
+        $viewVars = array();       
 
-        $viewVars = array();
+        if($this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            $user = $this->get('security.context')->getToken()->getUser();
 
-        if ($session->has('user')) {
-            $user = $session->get('user');
-            $viewVars['user'] = $user;
+            $viewVars['user']       = $user;
+            $viewVars['stations']   = $user->getStations(); 
         }
 
         return $this->render('ZubiIndexBundle:Default:index.html.twig', $viewVars);
     }
-    
 }
